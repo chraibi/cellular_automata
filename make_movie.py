8139b15ff4de15275751d5ecaa8f5452c4019d2b
Mohcine Chraibi <m.chraibi@gmail.com>
2014-12-03 00:01:12 +0100

    fix bug in make_movie

--- make_movie.py
+++ make_movie.py
@@ -1,19 +1,30 @@
 from sys import platform as _platform
 import os
 
+
 def make_movie(direct):
     files = os.path.join(direct, '*.png')
-    cmd = '"mf://%s" -mf w=800:h=600:fps=2:type=png -ovc lavc -lavcopts vcodec=mpeg4 -oac copy -o output.avi'%files
+    cmd = '"mf://%s" -mf w=800:h=600:fps=2:type=png -ovc lavc -lavcopts vcodec=mpeg4 -oac copy -o output.avi' % files
     os.system("mencoder %s" % cmd)
+
+
+def make_movie_win(direc):
+    print ('Windows not yet done!')
+
+
+def make_movie_osx(direct):
+    print ('OSX not yet done!')
 
 
 if __name__ == "__main__":
     directory = 'pngs'
+    if not os.path.exists(directory):
+        os.mkdir(directory)
     if _platform == "linux" or _platform == "linux2":
         make_movie(directory)
     elif _platform == "darwin":
-        print ('OSX not yet done!')
+        make_movie_osx(directory)
     elif _platform == "win32" or _platform == "cygwin":
-        print ('Windows not yet done!')
+        make_movie_win(directory)
     else:
         print('%s not known!' % _platform)
