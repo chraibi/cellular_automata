2086f24ab0002983cea6de3b34d5f402b1e1a0bf
carlos <ca2010es@gmail.com>
2016-09-07 13:11:28 +0200

    remove dead code

--- flow_eval.py
+++ flow_eval.py
@@ -10,6 +10,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import cellular_automaton as ca
 import pickle
+from time import time
 from multiprocessing import Pool
 
 
@@ -30,9 +31,9 @@ def pretty(var):
 
 class Config:
     # settings ------------------------------------------------------------------
-    ks = 2
+    ks = 0.4
     kd = 0
-    shuffle = False
+    shuffle = True
     reverse = False
     decay = 0.3 # delta
     diffusion = 0.1 # alpha
@@ -40,6 +41,7 @@ class Config:
     height = 25.2
     numPeds = 1116
     parallel = True
+    box = [1, 63, 1, 63]
 
     clean = False
     plotP = False
@@ -55,26 +57,24 @@ class Config:
 def time_var(var, values, nruns):
     Y = np.zeros(len(values) * nruns)
 
+    t = time()
     for i, x in enumerate(values):
-        nproc = min(nruns, 8)
+        conf = Config(nruns, **{var: x})
 
-        #njobs = [nruns // nproc] * nproc
-        #for i in range(nruns % nproc):
-        #    njobs[i] += 1
+        tt = ca.main(conf)
 
-        confs = [Config(nruns = 1, **{var: x}) for _ in range(nruns)]
-        with Pool(nproc) as pool:
-            tt = list(map(ca.main, confs))
-            print(tt)
         Y[i * nruns: (i + 1) * nruns] = tt
 
-        if conf.plotD or conf.plotAvgD:
+        if Config.plotD or Config.plotAvgD:
             os.system('rm -rf dff-{}'.format(i))
             os.system('mv dff dff-{}'.format(i))
 
-        if conf.plotP:
+        if Config.plotP:
             os.system('rm -rf peds-{}'.format(i))
             os.system('mv peds peds-{}'.format(i))
+
+    print('\n' + '-'*60)
+    print('total time: {:.2f} s'.format(time() - t))
 
     X = np.reshape([[x] * nruns for x in values], (i + 1) * nruns)
 
@@ -87,7 +87,7 @@ def time_var(var, values, nruns):
 
     if r / len(values) < 100:
         plt.plot(X, m * X + t)
-        plt.title(r't ~ {:.2f}s * N + {:.2f}s  |  Error: {:.2f}s'
+        plt.title(r't ~ {:.2f} s * N + {:.2f} s  |  Error: {:.2f} s'
                   .format(m, t, r[0] / len(values)))
     else:
         print('\n' + '-'*60)
@@ -96,11 +96,11 @@ def time_var(var, values, nruns):
         plt.plot(values, avgs)
 
     conf_dic = Config.__dict__.copy()
-    conf_dic.update(conf.__dict__)
+    conf_dic.update(Config.__dict__)
     conf_dic[var] = 'X'
 
     conf_str = CONF_STR.format(**conf_dic) + \
-               CONF_STR_OPT.format(**conf_dic) * (bool(conf.kd) or var == 'kd')
+               CONF_STR_OPT.format(**conf_dic) * (bool(Config.kd) or var == 'kd')
 
     plt.savefig('figs/evac_times{}.png'.format(conf_str))
 
@@ -108,13 +108,13 @@ def time_var(var, values, nruns):
         pickle.dump((X, Y), f) # use protocol = 2 for Python 2 compatibility
 
 if __name__ == '__main__':
-    ca.box = [1, 20, 1, 12]
+    ca.MAX_STEPS = 80000
 
     var = 'kd'
 
-    values = np.linspace(0, 3, 10)
+    values = np.linspace(0, 10, 50)
 
-    nruns = 3
+    nruns = 32
 
     time_var(var, values, nruns)
 
